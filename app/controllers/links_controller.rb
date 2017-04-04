043d9359825ab1690781059a62147f7bc0d08b2d
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-04-04 14:19:30 -0600

    Link create action

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,32 @@
 class LinksController < ApplicationController
 
+  before_action :set_link, :set_links
+
   def index
   end
+
+  def create
+    link = current_user.links.new(link_params)
+    if link.save
+      render partial: 'show', locals: {link: link}, layout: false
+    else
+      flash.now[:danger] = link.errors.full_messages.first
+      render partial: 'shared/flash'
+    end
+  end
+
+  private
+
+    def link_params
+      params.require(:link).permit(:title, :url)
+    end
+
+    def set_link
+      @link = Link.new
+    end
+
+    def set_links
+      @links = current_user.links
+    end
 
 end
